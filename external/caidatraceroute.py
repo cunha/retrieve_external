86dcf6171090475e1f2b62b6b94edca5a10c959a
Alex Marder <amarder89@gmail.com>
2018-12-11 16:39:38 -0500

    Both CAIDA traceroute types, team and prefix.

--- external/caidatraceroute.py
+++ external/caidatraceroute.py
@@ -1,7 +1,10 @@
+import re
 from datetime import timedelta
+from enum import Enum
 from multiprocessing.pool import Pool
 from os import makedirs
 from os.path import basename, join as pjoin
+from typing import Iterable, Tuple
 from urllib.parse import urlparse, urljoin
 
 import requests
@@ -11,6 +14,16 @@ from humanfriendly import format_size
 from progress.bar import Progress
 
 
+class TType(Enum):
+    """Traceroute types."""
+    team = 1
+    probing = 2
+
+
+class TTypeException(Exception):
+    """Raise for incorrectly supplied traceroute types."""
+
+
 class CaidaTraceroute:
 
     def __init__(self, args):
@@ -18,6 +31,7 @@ class CaidaTraceroute:
         self.auth = (args.username, args.password) if args.username else None
         self.begin = parse(args.begin)
         self.end = parse(args.end)
+        self.days = [self.begin + timedelta(i) for i in range((self.end - self.begin).days + 1)]
         self.processes = args.processes
         self.dir = args.dir
         if self.dir != '.' and self.dir != '..':
@@ -31,24 +45,7 @@ class CaidaTraceroute:
             f.write(content)
         return len(content)
 
-    def get(self):
-        days = [self.begin + timedelta(i) for i in range((self.end - self.begin).days + 1)]
-        urls = []
-        for day in days:
-            for team in range(1, 4):
-                url = ('https://topo-data.caida.org/team-probing/list-7.allpref24/team-{team}/daily/{year}/'
-                       'cycle-{year}{month:02d}{day:02d}/').format(
-                    team=team, year=day.year, month=day.month, day=day.day)
-                r = requests.get(url, auth=self.auth)
-                if r.ok:
-                    soup = BeautifulSoup(r.text, features="html.parser")
-                    pre = soup.find('pre')
-                    links = pre.find_all('a')
-                    for link in links:
-                        href = link['href']
-                        if href.startswith('daily'):
-                            joinedhref = urljoin(url, href)
-                            urls.append(joinedhref)
+    def parallel_download(self, urls):
         totalsize = 0
         pb = Progress(len(urls), 'Downloading traceroute files',
                       callback=lambda: 'Size {}'.format(format_size(totalsize)))
@@ -56,7 +53,52 @@ class CaidaTraceroute:
             for size in pb.iterator(pool.imap_unordered(self.download, urls)):
                 totalsize += size
 
+    def get(self, iterable: Iterable[Tuple[str, str]]):
+        urls = []
+        for url, regex in iterable:
+            regex = re.compile(regex)
+            r = requests.get(url, auth=self.auth)
+            if r.ok:
+                soup = BeautifulSoup(r.text, features="html.parser")
+                pre = soup.find('pre')
+                links = pre.find_all('a')
+                for link in links:
+                    href = link['href']
+                    if regex.search(href):
+                        joinedhref = urljoin(url, href)
+                        urls.append(joinedhref)
+        return urls
 
-def get_caidatraceroute(args):
+    def get_prefix(self):
+        for day in self.days:
+            url = ('https://topo-data.caida.org/prefix-probing/'
+                   '{year}/{month:02d}/').format(year=day.year, month=day.month)
+            regex = r'.*{year}{month:02d}{day:02d}.*.warts.gz'.format(year=day.year, month=day.month, day=day.day)
+            yield url, regex
+
+    def get_team(self):
+        for day in self.days:
+            for team in range(1, 4):
+                url = ('https://topo-data.caida.org/team-probing/list-7.allpref24/team-{team}/daily/{year}/'
+                       'cycle-{year}{month:02d}{day:02d}/').format(
+                    team=team, year=day.year, month=day.month, day=day.day)
+                yield url, r'.*\.warts\.gz'
+
+
+def get(args, ttype):
     ct = CaidaTraceroute(args)
-    ct.get()
+    if ttype == TType.team:
+        urls = ct.get(ct.get_team())
+    elif ttype == TType.probing:
+        urls = ct.get(ct.get_prefix())
+    else:
+        raise TTypeException('Invalid TType')
+    ct.parallel_download(urls)
+
+
+def get_caidateam(args):
+    get(args, TType.team)
+
+
+def get_caidaprefix(args):
+    get(args, TType.probing)
--- retrieve.py
+++ retrieve.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python
 from argparse import ArgumentParser
 
-from external.caidatraceroute import get_caidatraceroute
+from external.caidatraceroute import get_caidateam, get_caidaprefix
 
 
 def main():
     parser = ArgumentParser()
+    parser.add_argument('-b', '--begin', help='Beginning date.', required=True)
+    parser.add_argument('-e', '--end', help='Ending date.', required=True)
+    parser.add_argument('-u', '--username', help='Username.')
+    parser.add_argument('-p', '--password', help='Password.')
+    parser.add_argument('-n', '--processes', type=int, default=5, help='Number of processes to use.')
+    parser.add_argument('-d', '--dir', default='.', help='Output directory for the traceroute files.')
     sub = parser.add_subparsers()
-    caida = sub.add_parser('caida', help='Retrieve the CAIDA traceroutes.')
-    caida.add_argument('-b', '--begin', help='Beginning date.', required=True)
-    caida.add_argument('-e', '--end', help='Ending date.', required=True)
-    caida.add_argument('-u', '--username', help='CAIDA username')
-    caida.add_argument('-p', '--password', help='CAIDA password')
-    caida.add_argument('-n', '--processes', type=int, default=5, help='Number of processes to use.')
-    caida.add_argument('-d', '--dir', default='.', help='Output directory for the traceroute files.')
-    caida.set_defaults(func=get_caidatraceroute)
+
+    caidat = sub.add_parser('caida-team', help='Retrieve the CAIDA team probing traceroutes.')
+    caidat.set_defaults(func=get_caidateam)
+
+    caidap = sub.add_parser('caida-prefix', help='Retrieve the CAIDA prefix probing traceroutes.')
+    caidap.set_defaults(func=get_caidaprefix)
+
     args = parser.parse_args()
     args.func(args)
 
